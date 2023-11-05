d0eb423c3816daea44f801fb2fcb55b9b8f11a6a
Will Bolton <git@bolton.me.uk>
2023-11-05 12:36:32 +0000

    Read message and key from files
    It's much more robust to read from files as any non-ascii byte is going
    to be a nightmare to type in - likewise for large messages/keys. Plus
    messages can then be read from stdin almost for free.

--- otp.c
+++ otp.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
-#include <string.h>
 
-int cipher(char* message, char* key){
-	if (strlen(message) > strlen(key)){
-		printf("key must be as long as message\n");
-		return 1;
-	}
-
-	for(int i = 0; i<strlen(message); i++){
-		putchar(message[i] ^ key[i]);
+int cipher(FILE *message, FILE *key){
+	char m, k;
+	while ((m = getc(message)) != EOF && (k = getc(key)) != EOF){
+		putchar(m ^ k);
 	}
 
 	return 0;
 }
 
 int main(int argc, char **argv){
-	char *message;
-	char *key;
-
 	if (argc < 3) {
 		printf("not enough args\n");
 		return 1;
 	}
 
-	message = *(argv + 1);
-	key = *(argv + 2);
+	char *messagefilename = *(argv + 1);
+	char *keyfilename = *(argv + 2);
 
-	return cipher(message, key);
+	FILE *messagefile = fopen(messagefilename, "r");
+	FILE *keyfile = fopen(keyfilename, "r");
+
+	if (messagefile == 0){
+		printf("could not open message file %s\n", messagefilename);
+		return 2;
+	}
+
+	if (keyfile == 0){
+		printf("could not open key file %s\n", keyfilename);
+		return 2;
+	}
+
+	return cipher(messagefile, keyfile);
 }
