4d2a9d6fc04b6e2051585f93b7781d05fee2e31c
Will Bolton <git@bolton.me.uk>
2023-11-06 16:10:22 +0000

    Format otp.c

--- otp.c
+++ otp.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int cipher(FILE *message, FILE *key){
+int cipher(FILE *message, FILE *key)
+{
 	int m, k;
-	while ((m = getc(message)) != EOF){
+	while ((m = getc(message)) != EOF) {
 		k = getc(key);
-		if (k == EOF){
+		if (k == EOF) {
 			fprintf(stderr, "ERROR: key shorter than message.\nOutput has been truncated!\n");
 			return 1;
 		}
@@ -14,7 +15,8 @@ int cipher(FILE *message, FILE *key){
 	return 0;
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 	if (argc < 3) {
 		fprintf(stderr, "not enough args\n");
 		return 1;
@@ -26,12 +28,12 @@ int main(int argc, char **argv){
 	FILE *messagefile = fopen(messagefilename, "r");
 	FILE *keyfile = fopen(keyfilename, "r");
 
-	if (messagefile == 0){
+	if (messagefile == 0) {
 		fprintf(stderr, "could not open message file %s\n", messagefilename);
 		return 2;
 	}
 
-	if (keyfile == 0){
+	if (keyfile == 0) {
 		fprintf(stderr, "could not open key file %s\n", keyfilename);
 		return 2;
 	}
